b3937557d5bde5de8ca23fb1e37bdee83969f4b3
Salin Villamizar <aprendiz.sena.1@tiresia.co>
2025-01-08 17:03:19 -0500

    Arrays

---
+++ JAVA/Proyecto_java_inicial/src/main/java/com/mycompany/proyecto_java_inicial/Arrays.java
@@ -0,0 +1,19 @@
+
+package com.mycompany.proyecto_java_inicial;
+
+import static java.time.Instant.now;
+
+/**
+ *
+ * @author TIRESIA
+ */
+public class Arrays {
+    public static void main(String[] args) {
+        int vector [] = now int [4];
+        
+        vector[0]=1;
+        vector[1]=2;
+        vector[3]=3;
+        vector[4]=3;
+    }
+}
