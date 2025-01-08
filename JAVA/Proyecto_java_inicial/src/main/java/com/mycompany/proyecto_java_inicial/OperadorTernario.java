a04d4b37b21b2f5b59cff5fd9ea1c7d65cf8644c
Salin Villamizar <aprendiz.sena.1@tiresia.co>
2025-01-08 13:06:25 -0500

    JAVA

---
+++ JAVA/Proyecto_java_inicial/src/main/java/com/mycompany/proyecto_java_inicial/OperadorTernario.java
@@ -0,0 +1,14 @@
+package com.mycompany.proyecto_java_inicial;
+import java.util.Scanner;
+
+public class OperadorTernario {
+    public static void main(String[] args){
+        double promedio;
+        String conficionFinal;        
+        Scanner teclado = new Scanner(System.in);
+        System.out.println("Ingrese el promedio: ");
+        promedio = teclado.nextDouble();        
+        conficionFinal = promedio >=6 ? "Aprobado" : "Desaprobado";
+        System.out.println("La condicion final del alumno es: " + conficionFinal);
+    }
+}
--- JAVA/Proyecto_java_inicial/src/main/java/com/mycompany/proyecto_java_inicial/Proyecto_java_inicial.java
+++ JAVA/Proyecto_java_inicial/src/main/java/com/mycompany/proyecto_java_inicial/Proyecto_java_inicial.java
@@ -1,40 +1,32 @@
-
 package com.mycompany.proyecto_java_inicial;
-
 import java.util.Scanner;
 
 public class Proyecto_java_inicial {
-
     public static void main(String[] args) {
-
-        int rol = 3;
-        
+        int rol ;        
         Scanner teclado = new Scanner (System.in);
-        
+        rol = teclado.nextInt();        
         double sueldo1 = 15890; 
         double sueldo2 = 25630.89;
         double sueldo3 = 35560.20;
         double pago;
-        double pagototal;
-        
+        double pagototal;        
         switch (rol) {
-            case 1: 
-            pago = sueldo1 * 0.1;
-            pagototal = pago + sueldo1;
-            System.out.println("El pago total del repositor  es de: " + pagototal);
-                break;
-            case 2: 
-            pago = sueldo2;
-            System.out.println("El pago total del cajero  es de: " + pago);
-                break;
-            case 3: 
-            pago = sueldo3 * 0.11;
-            pagototal = sueldo3 - pago;
-            System.out.println("El pago total del supervisor  es de: " + pagototal);
-                break;
-            default:
-                System.out.println("El tipo de empleado ingresado es incorrecto");        
+            case 1 -> {
+                pago = sueldo1 * 0.1;
+                pagototal = pago + sueldo1;
+                System.out.println("El pago total del repositor  es de: " + pagototal);
             }
-        
+            case 2 -> {
+                pago = sueldo2;
+                System.out.println("El pago total del cajero  es de: " + pago);
+            }
+            case 3 -> {
+                pago = sueldo3 * 0.11;
+                pagototal = sueldo3 - pago;
+                System.out.println("El pago total del supervisor  es de: " + pagototal);
+            }
+            default -> System.out.println("El tipo de empleado ingresado es incorrecto");        
+            }
     }
 }
