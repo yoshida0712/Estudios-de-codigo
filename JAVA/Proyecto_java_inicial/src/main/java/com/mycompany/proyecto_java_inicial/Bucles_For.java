e75bbd4f296e2874e65dea3ad6980de9acda20ad
Salin Villamizar <aprendiz.sena.1@tiresia.co>
2025-01-08 15:33:03 -0500

    Bucle FOR

--- JAVA/Proyecto_java_inicial/src/main/java/com/mycompany/proyecto_java_inicial/Bucles_For.java
+++ JAVA/Proyecto_java_inicial/src/main/java/com/mycompany/proyecto_java_inicial/Bucles_For.java
@@ -3,8 +3,8 @@ package com.mycompany.proyecto_java_inicial;
 
 public class Bucles_For {
     public static void main(String[] args) {
-        for (int cont =20; cont<=10; cont--){
-            System.out.println("La condicion final del alumno es: " + cont);
+        for (int cont =0; cont<=10; cont++){
+            System.out.println("Doy vueltas con FOR, y voy en la: " + cont);
         }
     }
 }
