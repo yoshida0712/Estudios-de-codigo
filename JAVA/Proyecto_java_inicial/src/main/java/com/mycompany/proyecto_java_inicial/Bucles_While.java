0afa059d708091cf056dfb02fed2dd96c5b8ae49
Salin Villamizar <aprendiz.sena.1@tiresia.co>
2025-01-08 13:38:27 -0500

    While

---
+++ JAVA/Proyecto_java_inicial/src/main/java/com/mycompany/proyecto_java_inicial/Bucles_While.java
@@ -0,0 +1,13 @@
+package com.mycompany.proyecto_java_inicial;
+
+public class Bucles_While {
+    public static void main(String[] args) {
+        int cont = 0;
+        while (cont <=10){
+            System.out.println("La condicion final del alumno es: " + cont);
+            cont = cont + 1;
+        }
+    }
+    
+    
+}
