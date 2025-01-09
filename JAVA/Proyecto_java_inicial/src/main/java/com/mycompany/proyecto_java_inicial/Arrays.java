27eb6d9f57829deb3e6bf81f38ff6f9695843df4
Salin Villamizar <aprendiz.sena.1@tiresia.co>
2025-01-09 08:37:44 -0500

    Arrays

--- JAVA/Proyecto_java_inicial/src/main/java/com/mycompany/proyecto_java_inicial/Arrays.java
+++ JAVA/Proyecto_java_inicial/src/main/java/com/mycompany/proyecto_java_inicial/Arrays.java
@@ -1,7 +1,7 @@
 
 package com.mycompany.proyecto_java_inicial;
 
-import static java.time.Instant.now;
+import java.util.Scanner;
 
 /**
  *
@@ -9,11 +9,19 @@ import static java.time.Instant.now;
  */
 public class Arrays {
     public static void main(String[] args) {
-        int vector [] = now int [4];
+        int vector[] = new int[4];
         
-        vector[0]=1;
-        vector[1]=2;
-        vector[3]=3;
-        vector[4]=3;
+        Scanner teclado = new Scanner (System.in);    
+        
+        for (int i = 0; i < vector.length; i++) {
+            System.out.println("Ingrese un numero para el vector");
+            vector[i] = teclado.nextInt();
+         }
+        
+        for (int i = 0; i < vector.length; i++) {
+            System.out.println("Estoy en el indice: " + i);
+            System.out.println("Tengo guardado un " + vector[i]);
+            System.out.println("---------------------------------");
+        }
     }
 }
