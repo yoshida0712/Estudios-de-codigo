f2c6669c748d85ef91cc52d04e18ecb01695e598
Salin Villamizar <aprendiz.sena.1@tiresia.co>
2025-01-21 10:33:40 -0500

    Programacion oriendata a objetos

--- JAVA/Anfiteatro/src/anfiteatro/Anfiteatro.java
+++ JAVA/Anfiteatro/src/anfiteatro/Anfiteatro.java
@@ -12,12 +12,47 @@ public class Anfiteatro {
     public static void main(String[] args) {
 
         List<ticket> listaTicket = new ArrayList<ticket>();
+        Scanner teclado = new Scanner(System.in);
+        Scanner teclado2 = new Scanner(System.in);
+        Scanner teclado3 = new Scanner(System.in);
+        Scanner teclado4 = new Scanner(System.in);
+
+        boolean bandera2 = false;
+        
+        
+        while (!bandera2) {
+            int cont = 4;
+            System.out.println("Por favor ingrese la fila: ");
+            int fila = teclado2.nextInt();
+            System.out.println("Por favor ingrese el asiento: ");
+            int asiento = teclado2.nextInt();
+            System.out.println("Por favor ingrese el precio: ");
+            double precio = teclado3.nextDouble();
+            
+            
+            ticket ticketFila = new ticket();
+            ticketFila.setNumero(cont);
+            ticketFila.setFila(fila);
+            ticketFila.setAsiento(asiento);
+            ticketFila.setPrecio(precio);
+            ticketFila.setFechaCompra(new Date());
+            ticketFila.setFechaValidez(new Date());
+
+            listaTicket.add(ticketFila);
+            
+            System.out.println("Desea seguir ingresando tiquetes ? S/N");
+            String salir = teclado.next();
+            if (salir.equalsIgnoreCase("n")){
+                bandera2 = true;
+            }
+            cont = cont ++;
+        }
 
         ticket ticket1 = new ticket(1, 1, 1, 1500, new Date(), new Date());
         ticket ticket2 = new ticket(2, 1, 2, 1500, new Date(), new Date());
 
         ticket ticket3 = new ticket();
-        ticket3.setNumero(20);
+        ticket3.setNumero(3);
         ticket3.setFila(2);
         ticket3.setAsiento(3);
         ticket3.setPrecio(1500);
@@ -36,14 +71,13 @@ public class Anfiteatro {
 
         System.out.println("La suma de los precios de los tickets es: " + suma);
 
-        Scanner teclado = new Scanner(System.in);
         System.out.println("Ingrese la fila que desea ver: ");
-        int fila = teclado.nextInt();
+        int fila2 = teclado2.nextInt();
 
         boolean bandera = false;
 
         for (ticket ticket : listaTicket) {
-            if (ticket.getFila() == fila) {
+            if (ticket.getFila() == fila2) {
                 System.out.println(ticket.toString());
                 bandera = true;
             }
