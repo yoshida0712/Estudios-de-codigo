5ab2c8c6209a7830880b2d28341a28e60459afaf
Salin Villamizar <aprendiz.sena.1@tiresia.co>
2025-01-28 08:36:10 -0500

    POO

--- JAVA/Proyecto_java_inicial/src/main/java/Collections/LINKEDLIST.java
+++ JAVA/Proyecto_java_inicial/src/main/java/Collections/LINKEDLIST.java
@@ -1,25 +1,23 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package Collections;
 
 import java.util.LinkedList;
 import java.util.List;
 
-/**
- *
- * @author TIRESIA
- */
 public class LINKEDLIST {
     public static void main(String[] args){
         List<Persona> lista = new LinkedList<Persona> ();
         
         lista.add(new Persona(1,"David",21));
         lista.add(new Persona(5,"Samuel",22));
-        lista.add(new Persona(3,"Camilo",23));
+        lista.add(new Persona(3,"Camilo",23));        
         lista.add(new Persona(4,"Andres",24));
-        //No tienen un indice bien definido
+        
+        //agregar al inicio de la lista
+        //
+        lista.add(0, new Persona(6,"Manuel",30));
+        
+        //No tienen un indice bien definido, por eso se debe usar un for each para recorer cada dato
         for (Persona perso1:lista){            
             System.out.println("prueba: " + perso1.getNombre());
         }
